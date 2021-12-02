793411a221210eb0bfb45886068b7106f02e5956
paraspatle-jpg <paras18224827@gmail.com>
2021-12-02 12:08:24 +0530

    initial commit

--- trie.cpp
+++ trie.cpp
@@ -7,12 +7,17 @@ string trie_names[] = {"paraspatle", "deeppanchani"};
 
 int main()
 {
- int size = sizeof(trie_names) / sizeof(trie_names[0]);
+ // creating an object of class trie_node for root
  trie_node *root = new trie_node();
+
+ //finding the size of array
+ int size = sizeof(trie_names) / sizeof(trie_names[0]);
  for (int i = 0; i < size; i++)
  {
   root->insert(trie_names[i]);
  }
+
+ //searching name of user
  string name;
  cout << "Enter Your Name to search it in the trie : ";
  cin >> name;
